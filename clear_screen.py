a82bbda1334048e15f49bda2df8360b0ba597466
Odden69 <helena.joh2@gmail.com>
2021-12-04 10:06:22 +0000

    Replace clear function

--- clear_screen.py
+++ clear_screen.py
@@ -8,4 +8,10 @@ import os
 
 
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    # os.system('cls' if os.name == 'nt' else 'clear')
+    # os.system(clear-history)
+    # os.system('cls||clear')
+    # print(chr(27) + "[2J")
+    os.system('printf "\ec"')
+    # clear && printf '\e[3J'
+    # print('\x1b[2J')
