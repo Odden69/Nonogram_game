371533979f4d54aa4abdcf24af5361f994b47b14
Odden69 <helena.joh2@gmail.com>
2021-12-04 10:15:39 +0000

    Delete outcommented code in clear_screen.py

--- clear_screen.py
+++ clear_screen.py
@@ -2,16 +2,10 @@
 Clears the terminal
 
 The code was found on
-https://stackoverflow.com/questions/2084508/clear-terminal-in-python
+https://askubuntu.com/questions/25077/how-to-really-clear-the-terminal
 """
 import os
 
 
 def clear_screen():
-    # os.system('cls' if os.name == 'nt' else 'clear')
-    # os.system(clear-history)
-    # os.system('cls||clear')
-    # print(chr(27) + "[2J")
     os.system('printf "\ec"')
-    # clear && printf '\e[3J'
-    # print('\x1b[2J')
